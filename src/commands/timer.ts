6f5b0319a90b2be7abb93cdd62d9717468bf644a
lestert <lester.tang@tbcollege.com>
2021-05-18 16:15:13 -0400

    fix timer command

--- src/commands/timer.ts
+++ src/commands/timer.ts
@@ -5,20 +5,18 @@ export const description = "set a timer to get pinged after time has elapsed";
 export const execute = async (
   message: Message,
   args: string[]
-): Promise<void | Message> => {
+): Promise<void | undefined | Message> => {
+  if (!args[0]) return;
   if (!args[0].match(/^[0-9]*$/))
     return message.channel.send("Please enter a valid number.");
-
-  if (!args[1]) {
-    return message.channel.send("Please include a message.");
-  }
+  if (!args[1]) return message.channel.send("Please include a message.");
 
   let duration: number = parseInt(args[0].replace(/\D/g, ""));
 
   if (duration > 60 || duration < 1)
     return message.channel.send("Please enter a number between 1 and 60.");
 
-  duration = duration * 1000;
+  duration = duration * 60000;
 
   const msg = `${message.member} ${args.slice(1).join(" ")}`;
 
